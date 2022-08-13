2b203b18248913dafce0e35a47c19c3d21ca5bdf
Marcin Szkudlinski <szkudlik@gmail.com>
2022-08-13 16:42:47 +0200

    move some logging defs to debug.h

--- src/common.h
+++ src/common.h
@@ -13,48 +13,6 @@
 #define CONTROLLER_DEVICE_ID 1
 #define DEVICE_ID_BROADCAST 0xFF
 
-
-
-#ifdef CONTROLLER
-
-//#define DEBUG_1
-//#define DEBUG_2
-#define DEBUG_3
-
-#define DEBUG_SERIAL Serial
-#define DEBUG_SERIAL_EVENT serialEvent
-
-#include "ResponseHandler.h"
-
-#endif //ifdef CONTROLLER
-
-
-
-
-#ifdef DEBUG_1
-#define DEBUG_PRINTLN_1(x) RespHandler.println(F(x));
-#else
-#define DEBUG_PRINTLN_1(x)
-#endif
-
-#ifdef DEBUG_2
-#define DEBUG_PRINTLN_2(x) RespHandler.println(F(x));
-#else
-#define DEBUG_PRINTLN_2(x)
-#endif
-
-#ifdef DEBUG_3
-#define DEBUG_PRINTLN_3(x) RespHandler.println(F(x));
-#else
-#define DEBUG_PRINTLN_3(x)
-#endif
-
-
-
-
-
-
-
-
+#include "debug.h"
 
 #endif
--- src/debug.h
+++ src/debug.h
@@ -8,6 +8,39 @@
 #ifndef SRC_DEBUG_H_
 #define SRC_DEBUG_H_
 
+#ifdef CONTROLLER
+
+//#define DEBUG_1
+//#define DEBUG_2
+#define DEBUG_3
+
+#define DEBUG_SERIAL Serial
+#define DEBUG_SERIAL_EVENT serialEvent
+
+#include "ResponseHandler.h"
+
+#endif //ifdef CONTROLLER
+
+
+
+
+#ifdef DEBUG_1
+#define DEBUG_PRINTLN_1(x) RespHandler.println(F(x));
+#else
+#define DEBUG_PRINTLN_1(x)
+#endif
+
+#ifdef DEBUG_2
+#define DEBUG_PRINTLN_2(x) RespHandler.println(F(x));
+#else
+#define DEBUG_PRINTLN_2(x)
+#endif
+
+#ifdef DEBUG_3
+#define DEBUG_PRINTLN_3(x) RespHandler.println(F(x));
+#else
+#define DEBUG_PRINTLN_3(x)
+#endif
 
 
 
