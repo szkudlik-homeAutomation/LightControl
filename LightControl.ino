be7172ed738e8a4e915b71c673113c600e847402
Marcin Szkudlinski <szkudlik@gmail.com>
2022-08-21 10:31:11 +0200

    Update TCP_Communication_lib

--- LightControl.ino
+++ LightControl.ino
@@ -13,7 +13,7 @@
 
 #ifdef CONTROLLER
 #include "src/servlets.h"
-#include "src/network.h"
+#include "src/TCP_Communication_lib/network.h"
 #include "src/TelnetServer.h"
 #include "src/TCP_Communication_lib/HttpServer.h"
 #include "src/LightWebControl.h"
