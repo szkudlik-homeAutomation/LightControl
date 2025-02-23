bdd073f828849b3c2d951615b9607efe28d04f8d
szkudlik <szkudlik@gmail.com>
2025-02-23 12:23:04 +0100

    get actions servlet - dummy data, syntax fix

--- src/LightControl/tLightControlgetActionsJSONservlet.cpp
+++ src/LightControl/tLightControlgetActionsJSONservlet.cpp
@@ -15,40 +15,28 @@ bool tLightControl_getActionsJSON_servlet::ProcessAndResponse()
  {
 	tMessageTypeSetAction action1 = {1,2,3,4,5,6,7,8};
 	tMessageTypeSetAction action2 = {10,20,30,40,50,60,70,80};
-	uint8_t num_of_devices = 2;
 	uint8_t num_of_actions = 2;
+	uint8_t j = 1;
 
-	pOwner->SendFlashString(PSTR("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{"));
+	pOwner->SendFlashString(PSTR("HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n"));
 
-	pOwner->SendFlashString(PSTR("\"Devices\":{"));
-	for (uint8_t j = 0; j < num_of_devices; j++) {
-		if (j > 0) {
-			pOwner->mEthernetClient.print(",");
+	pOwner->SendFlashString(PSTR("{\"DevID\":"));
+	pOwner->mEthernetClient.print(j, DEC);
+	pOwner->SendFlashString(PSTR(", \"Actions\":{"));
+	for (uint8_t i = 0; i < num_of_actions; i++) {
+		if (i > 0)
+		{
+			pOwner->SendFlashString(PSTR(","));
 		}
-
-		pOwner->mEthernetClient.print("\"");
-		pOwner->mEthernetClient.print(j, DEC);
-		pOwner->mEthernetClient.print("\":");
-
-		pOwner->SendFlashString(PSTR("{\"DevID\":"));
-		pOwner->mEthernetClient.print(j, DEC);
-		pOwner->SendFlashString(PSTR(", \"Actions\":{"));
-		for (uint8_t i = 0; i < num_of_actions; i++) {
-			if (i > 0) {
-				pOwner->SendFlashString(PSTR(","));
-			}
-			pOwner->SendFlashString(PSTR("\""));
-			pOwner->mEthernetClient.print(i, DEC);
-			pOwner->SendFlashString(PSTR("\":"));
-			if (i == 0) tMessageTypeSetActionToJSON(&action1);
-			if (i > 0) tMessageTypeSetActionToJSON(&action2);
-		}
-
-		pOwner->SendFlashString(PSTR("}}"));
+		pOwner->SendFlashString(PSTR("\""));
+		pOwner->mEthernetClient.print(i, DEC);
+		pOwner->SendFlashString(PSTR("\":"));
+		if (i == 0) tMessageTypeSetActionToJSON(&action1);
+		if (i > 0) tMessageTypeSetActionToJSON(&action2);
 	}
 	pOwner->SendFlashString(PSTR("}}"));
 
-return false;
+	return false;
 }
 
 void tLightControl_getActionsJSON_servlet::tMessageTypeSetActionToJSON(tMessageTypeSetAction *pAction)
